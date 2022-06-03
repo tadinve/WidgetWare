3e89260c4c95aa3af445cd6edd5e1e4c9ae32505
Venkatesh Tadinada <venkat@solivar.com>
2022-06-03 22:21:24 +0000

    made changes to start-airflow

--- start-airflow.sh
+++ start-airflow.sh
@@ -1,6 +1,6 @@
 ## Run the below build command for first run
 # docker-compose build --no-cache airflow
-
+docker-compose build --no-cache airflow
 docker-compose up -d postgres 
 docker-compose up -d postgres-dbt
 docker-compose up airflow
